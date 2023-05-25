a4544675a98daee5a4771268263767b061d595d9
Emily McLean <emilymclean93@gmail.com>
2023-05-25 18:13:03 -0400

    getDb route fixes

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -26,7 +26,7 @@ export const putDb = async (content) => {
   const store = tx.objectStore('jate');
 
   // Use the .add() method on the store and pass in the content.
-  const request = store.put({ id: body, value: content});
+  const request = store.put({ id: 'body', value: content});
 
   // Get confirmation of the request.
   const result = await request;
@@ -47,7 +47,7 @@ export const getDb = async () => {
   const store = tx.objectStore('jate');
 
   // Use the .getAll() method to get all data in the database.
-  const request = store.getAll();
+  const request = store.get('body');
 
   // Get confirmation of the request.
   const result = await request;
